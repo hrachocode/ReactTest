f122605664f16d6b9744ff7843d93ffbee4bebf4
davk01 <davitwjob@gmail.com>
2019-12-05 13:54:26 +0400

    Todo List App Final

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,11 +2,35 @@ import React,{Component} from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+    state = {
+        label: ''
+    }
+    onLabelCHange = (event) => {
+       this.setState({
+           label:event.target.value,
+          
+       })
+     
+    }
+    onSubmit = (event) => {
+        event.preventDefault()
+        this.props.onItemAdded(this.state.label)
+        this.setState({
+            label: ''
+        })
+       
+    }
     render(){
         return (
-            <div className="item-add-form">
-                <button className="btn btn-outline-secondary" onClick={() => this.props.onItemAdded('helo')}>Add Item</button>
-            </div>
+            <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+                <input type="text"
+                       className="form-control"
+                       placeholder="what needs to be done" 
+                       onChange={this.onLabelCHange} 
+                       value={this.state.label}
+                ></input>
+                <button className="btn btn-outline-secondary" type="submit">Add Item</button>
+            </form>
         )
     }
 }
